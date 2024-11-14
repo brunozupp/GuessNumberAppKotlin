c36a822e171329b133ce7812b27645c49af63e3e
Bruno Noveli <brunimzuppwolf@gmail.com>
2024-11-14 15:49:25 -0300

    Added showSnackbar to indicate the result of the choosing number

--- app/src/main/java/com/novelitech/guessnumberappkotlin/GuessNumber.kt
+++ app/src/main/java/com/novelitech/guessnumberappkotlin/GuessNumber.kt
@@ -5,35 +5,53 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.novelitech.guessnumberappkotlin.components.BoxNumber
 import com.novelitech.guessnumberappkotlin.components.DisplayInformation
 import com.novelitech.guessnumberappkotlin.components.KeyboardGuessNumbers
+import kotlinx.coroutines.launch
 import kotlin.random.Random
 
 @Composable
 fun GuessNumber(modifier: Modifier = Modifier) {
 
+    // Here I don't delegate the setter/getter to anyone by using the 'by' keyword because
+    // I just want to use it to read and save its value when rendering the page
     val numberToGuess = remember {
         Random.nextInt(1, 7)
     }
 
+    // Here I delegate the setter/getter to the remember because it will be the responsible for
+    // retrieve and update the value from mutableIntStateOf
     var attemptsLeft by remember {
         mutableIntStateOf(3)
     }
+
+    var winTheGame by remember {
+        mutableStateOf<Boolean?>(null)
+    }
+
+    // Variables responsible for the Snackbar
+
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
 
     println("The number I need to guess is $numberToGuess")
 
     Scaffold(
         modifier = modifier
-            .background(Color.White)
+            .background(Color.White),
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
     ) {
         Column(
             modifier = Modifier
@@ -58,18 +76,43 @@ fun GuessNumber(modifier: Modifier = Modifier) {
 
                     attemptsLeft -= 1
 
-//                    if(number == numberToGuess) {
-//                        // State Success
-//                    } else {
-//
-//                        if(attemptsLeft > 0) {
-//                            // Show Error Guess and Try Again
-//                        } else {
-//                            // Show Error State
-//                        }
-//                    }
+                    if(number == numberToGuess) {
+
+                        scope.launch {
+                            snackbarHostState.showSnackbar(
+                                "You guessed correct. Number $numberToGuess is the guessing number.",
+                                withDismissAction = true,
+                            )
+                        }
+
+                        winTheGame = true
+                    } else {
+
+                        if(attemptsLeft > 0) {
+
+                            scope.launch {
+                                snackbarHostState.showSnackbar(
+                                    "You didn't guess the correct one, try again. $attemptsLeft attempts is left.",
+                                    withDismissAction = true,
+                                )
+                            }
+
+                        } else {
+
+                            scope.launch {
+                                snackbarHostState.showSnackbar(
+                                    "All your attempts were used, you LOST.",
+                                    withDismissAction = true,
+                                )
+                            }
+
+                            winTheGame = false
+                        }
+                    }
                 }
             )
         }
     }
+
+
 }
